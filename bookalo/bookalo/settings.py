8bf6fcaae2d8087fce1f6733654e4f101448e5b1
Nacpac <nachoooxd98@gmail.com>
2019-03-17 19:56:11 +0100

    Update settings.py

--- bookalo/bookalo/settings.py
+++ bookalo/bookalo/settings.py
@@ -22,10 +22,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ctdnr@)8&ms1*ecmoi4kvjt_37qm#8os_-mcnjb&c1pa4(9naw'
 
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY ='797410121075-gmkfdson9hld1474iq8i6jq7jcs39ps6.apps.googleusercontent.com '
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'TEMkYL9qVHJF8lZw2Q3y0JgF' 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+LOGIN_URL = 'login'   #Esto ponerlo bien cuando lo tengamos
+LOGIN_REDIRECT_URL = 'home' #Esto ponerlo bien cuando lo tengamos
 
 
 # Application definition
@@ -69,6 +75,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -78,6 +86,9 @@ WSGI_APPLICATION = 'bookalo.wsgi.application'
 
 
 AUTHENTICATION_BACKENDS = (
+    'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+	'social_core.backends.google.GoogleOpenId',  # for Google authentication
+	'social_core.backends.google.GoogleOAuth2',  # for Google authentication
     "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
 )
