4582a3de4d294ba93cb99dad8150dc253938b858
Nacpac <nachoooxd98@gmail.com>
2019-04-08 23:01:08 +0200

    añadida una cosa

--- bookalobackend/bookalo/views.py
+++ bookalobackend/bookalo/views.py
@@ -178,15 +178,14 @@ def GetUserProducts(request, format=None):
 		try:
 			user_info = auth.get_account_info(token)
 			user_uid = user_info['users'][0]['localId']
+			user = Usuario.objects.get(uid=user_uid)
+			products = Producto.objects.filter(vendido_por=user)
+			serializer = ProductoSerializerList(products, many=True, read_only=True)
+			return Response(serializer.data, status=status.HTTP_200_OK)
 		except:
 			return Response(status=status.HTTP_404_NOT_FOUND)
 
-		user = Usuario.objects.get(uid=user_uid)
-		products = Producto.objects.filter(vendido_por=user)
-		serializer = ProductoSerializerList(products, many=True, read_only=True)
-		return Response(serializer.data, status=status.HTTP_200_OK)
-
-
+		
 
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
