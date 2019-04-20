285779556922bb4879b9c7f888bab9ca3a74d534
Enrique Torres <etsmsj@hotmail.com>
2019-04-20 12:26:16 +0200

    reorganizacion del repositorio y eliminacion de ficheros innecesarios

--- init_postgre.sh
+++
@@ -1,8 +0,0 @@
-createdb bookalo
-createuser bookalo_admin
-psql <<EOF
-ALTER USER bookalo_admin WITH PASSWORD 'bookalomaster';
-GRANT ALL PRIVILEGES ON DATABASE bookalo TO bookalo_admin;
-\q
-EOF
-exit
--- setup_postgre.sh
+++
@@ -1,6 +0,0 @@
-set -e
-sudo apt-get update
-sudo apt-get install libpq-dev postgresql postgresql-contrib
-pip3 install psycopg2
-sudo su - postgres < ./init_postgre.sh
-echo "Preparacion de PostgreSQL para el proyecto Bookalo terminada"
