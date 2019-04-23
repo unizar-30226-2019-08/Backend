8bdb5582bccd25ab74698d5a7df0d8dcb3102a42
pitazzo <pedromalo@live.com>
2019-04-23 02:07:10 +0200

    Script deploy actualizado

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,12 +4,11 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/bookalo_key # Allow read access to the private key
 ssh-add .travis/bookalo_key # Add the private key to SSH
 
+ssh roberta@bookalo.es "sudo systemctl stop uwsgi"
+
 git config --global push.default matching
 git remote add deploy ssh://git@$IP$DEPLOY_DIR
 git push deploy master
 
-# Skip this command if you don't need to execute any additional commands after deploying.
-ssh roberta@$IP <<EOF
-  cd $DEPLOY_DIR
-  touch test_fue_bien
-EOF
+ssh roberta@bookalo.es "source bin/activate && python src/manage.py collectstatic && deactivate"
+ssh roberta@bookalo.es "sudo systemctl start uwsgi"
