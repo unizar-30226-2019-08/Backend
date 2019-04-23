2de135b4ee297198daf96e668ade23b96de17de5
pitazzo <pedromalo@live.com>
2019-04-23 13:47:47 +0200

    Actualización deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,7 +8,7 @@ ssh roberta@bookalo.es "sudo systemctl stop uwsgi"
 
 git config --global push.default matching
 git remote add deploy ssh://git@$IP$DEPLOY_DIR
-git push deploy master
+git push -f deploy master
 
 ssh roberta@bookalo.es "source bin/activate && python src/manage.py collectstatic --noinput && python src/manage.py makemigrations && python src/manage.py migrate --syn-db && deactivate"
 ssh roberta@bookalo.es "sudo systemctl start uwsgi"
