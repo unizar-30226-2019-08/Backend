9eeb73f403aa9f03ae71fb64c8527ddbdb9897d8
Deivion <you@example.com>
2019-05-08 10:46:55 +0200

    Issue David

--- bookalo/views.py
+++ bookalo/views.py
@@ -848,7 +848,8 @@ def Vender_producto(request, format=None):
 			else:
 				# Se ha podido marcar como vendido
 				# Se crea instancia
-	
+
+				Mensaje = "Se ha vendido el producto: "	+chat_buscado.producto.nombre	
 				user1 = chat_buscado.vendedor
 				user2 = chat_buscado.comprador
 
