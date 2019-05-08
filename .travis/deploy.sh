0d5bb3fa0ffbe93bc83210b267055339bd1b86e6
pitazzo <pedromalo@live.com>
2019-05-08 22:35:42 +0200

    Configuración pip

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,5 +10,6 @@ git config --global push.default matching
 git remote add deploy ssh://git@$IP$DEPLOY_DIR
 git push deploy master
 
+ssh roberta@bookalo.es "source bin/actiavte && pip install -r requirements.txt && deactivate"
 ssh roberta@bookalo.es "source bin/activate && python src/manage.py collectstatic --noinput && python src/manage.py makemigrations && python src/manage.py migrate --run-syncdb && deactivate"
 ssh roberta@bookalo.es "sudo systemctl start uwsgi"
