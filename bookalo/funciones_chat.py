e42f07a60d309829b551220a0f3bac9a6a4e629c
Nacpac <nachoooxd98@gmail.com>
2019-05-27 11:16:25 +0200

    chat

--- bookalo/funciones_chat.py
+++ bookalo/funciones_chat.py
@@ -227,6 +227,21 @@ def SendFCMMessage(chat_id, message, token_emisor, emisor, soy_vendedor, recepto
 		tokens_emisor = get_list_tokens(emisor, token_emisor)
 		if tokens_emisor['movil']:
 			data = {
+				"notification":{
+					"title":"Bookalo",
+					"body":"¡Hola " + emisor.nombre + "! La venta se ha cerrado para el producto " + chat_obj.producto.nombre + ". ¡Valora a " + receptor.nombre + "!",
+					"icon":"https://bookalo.es/media/bookalo_logo.png"
+				},
+				"registration_ids":tokens_emisor['movil'],
+				"data":{
+					"chat":chat,
+					"soy_vendedor":not soy_vendedor,
+					"mensaje":mensaje,
+				}
+			}
+			data = json.dumps(data)
+			requests.post(url=URL, data=data, headers=headers)
+			data = {
 				"registration_ids":tokens_emisor['movil'],
 				"data":{
 					"chat":chat,
